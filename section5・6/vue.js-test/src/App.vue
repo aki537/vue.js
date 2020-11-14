f35866fd1ce3a5ea2cfdc24a7ab409fd67af7a89
aki158 <hideponpon1931@gmail.com>
2020-11-14 09:11:04 +0900

    78 単一ファイルコンポーネント（vueファイル)の基礎的な使い方

--- section5・6/vue.js-test/src/App.vue
+++ section5・6/vue.js-test/src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <p>いいね({{number}})</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    // ES6はこの書き方
-    // ES5の HelloWorld: HelloWorldと同じ意味
-    HelloWorld
+  // data: function() {
+    data() {
+    return {
+      number: 5
+    };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
