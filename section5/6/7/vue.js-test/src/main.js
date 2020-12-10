b95adaaad2b0c89e473ab1f18e4bf8d75d8a7b8b
aki158 <hideponpon1931@gmail.com>
2020-12-10 15:26:03 +0900

    133 ディレクティブはどのように動くのか

--- section5/6/7/vue.js-test/src/main.js
+++ section5/6/7/vue.js-test/src/main.js
@@ -9,11 +9,21 @@ Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber)
 
 Vue.directive("border", {
-  bind() {},
-  insert() {},
-  update() {},
-  componentUpdate() {},
-  unbind() {},
+  bind() {
+    // ディレクティブで初めて対象の要素に紐付いたとき
+  },
+  insert() {
+    // 親Nodeに挿入されたとき
+  },
+  update() {
+    // コンポーネントが更新される度、子コンポーネントが更新される前
+  },
+  componentUpdate() {
+    // コンポーネントが更新される度。子コンポーネントが更新された後
+  },
+  unbind() {
+    // ディレクティブが紐付いている要素から取り除かれたとき
+  },
 });
 
 new Vue({
