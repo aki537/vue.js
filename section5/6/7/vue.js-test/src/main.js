616c22969a0fe4a6566db2f33a89cbc9865706b6
aki158 <hideponpon1931@gmail.com>
2020-12-16 22:57:33 +0900

    140 修飾子をカスタムディレクティブに対して使えるようにする

--- section5/6/7/vue.js-test/src/main.js
+++ section5/6/7/vue.js-test/src/main.js
@@ -13,26 +13,13 @@ Vue.directive("border", function(el, binding){
   el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
   el.style.borderStyle = binding.arg;
+  if (binding.modifiers.round) {
+    el.style.borderRadius = "0.5rem";
+  }
+  if (binding.modifiers.shadow) {
+    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+  }
 });
-
-// {
-//   bind() {
-//     // ディレクティブで初めて対象の要素に紐付いたとき
-//   },
-//   // insert() {
-//   //   // 親Nodeに挿入されたとき
-//   // },
-//   update() {
-//     // コンポーネントが更新される度、子コンポーネントが更新される前
-//   },
-//   // componentUpdate() {
-//   //   // コンポーネントが更新される度。子コンポーネントが更新された後
-//   // },
-//   // unbind() {
-//   //   // ディレクティブが紐付いている要素から取り除かれたとき
-//   // },
-// }
-
 
 new Vue({
   render: h => h(App),
