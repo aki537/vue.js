c8c59c714687d343b39f24ac5c9343629e4a7ea9
aki158 <hideponpon1931@gmail.com>
2020-12-23 15:43:03 +0900

    151 ミックスインを使ってオプションをコンポーネント間で共有する

--- section5/6/7/vue.js-test/src/components/CountNumber.vue
+++ section5/6/7/vue.js-test/src/components/CountNumber.vue
@@ -8,18 +8,9 @@
 </template>
 
 <script>
+import { tokyoNumber } from "@/tokyoNumber"
+
 export default {
-  data() {
-    return {
-      title: "Welcome to Tokyo",
-      subTitle: "Tokyo is a great city",
-      number: 0
-    };
-  },
-  filters: {
-    lowerCase(value) {
-      return value.toLowerCase();
-    }
-  },
-}
+  mixins: [tokyoNumber]
+};
 </script>
