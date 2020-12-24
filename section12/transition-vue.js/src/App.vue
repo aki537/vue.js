7c6f8de26261738f3a5e0ae4245ba112fa5d66ad
aki158 <hideponpon1931@gmail.com>
2020-12-24 11:15:37 +0900

    159 6つのtransitionクラスの役割

--- section12/transition-vue.js/src/App.vue
+++ section12/transition-vue.js/src/App.vue
@@ -21,22 +21,22 @@ export default {
 <style scoped>
 /* transitionでつけたnameで以下の6つのクラスを作る */
 .fade-enter {
-
+  /* 現れるときの最初の状態 */
 }
 .fade-enter-active {
-
+  /* 現れるときのトランジションの状態 */
 }
 .fade-enter-to {
-
+  /* 現れるときの最後の状態 */
 }
 .fade-leave {
-
+  /* 消えるときの最初の状態 */
 }
 .fade-leave-active {
-
+  /* 消えるときのトランジョンの状態 */
 }
 .fade-leave-to {
-
+  /* 消えるときの最後の状態 */
 }
 
 .main {
