8e05f6dc73c88ed4038234d8415b53d6ddf1f0dc
aki158 <hideponpon1931@gmail.com>
2020-12-24 13:03:38 +0900

    166 カスタムトランジションクラスを使ってanimate.cssを使用する

--- section12/transition-vue.js/src/App.vue
+++ section12/transition-vue.js/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="main">
     <button @click="show = !show">切り替え</button>
-    <transition name="fade" appear>
+    <transition
+      enter-active-class="animated bounce"
+      leave-active-class="animated shake"
+      appear
+    >
       <p v-if="show">hello</p>
     </transition>
     <!-- appearをつけると初期描画するときもトランジションが実行去れる -->
