c995ddcd8270c384d02de8e59096348894b59a7d
aki158 <hideponpon1931@gmail.com>
2020-12-29 11:46:16 +0900

    177 transition-groupを使ってリストトランジションを作成

--- section12/transition-vue.js/src/App.vue
+++ section12/transition-vue.js/src/App.vue
@@ -3,6 +3,19 @@
     <button @click="myAnimation= 'slide'">Slide</button>
     <button @click="myAnimation= 'slide'">Fade</button>
     <p>{{ myAnimation }}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group>
+        <li
+          style="cursor: pointer;"
+          v-for="(number) in numbers"
+          @click="remove(index)"
+          :key="number"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
     <br><br>
     <transition
@@ -60,12 +73,24 @@ export default {
   },
   data() {
     return {
+      numbers: [0, 1, 2],
+      nextNumber: 3,
       show: true,
       myAnimation: 'slide',
       myComponent: "ComponentA"
     };
   },
   methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length)
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
     beforeEnter(el) {
       // 現れる前
       el.style.transform = 'scale(0)'
