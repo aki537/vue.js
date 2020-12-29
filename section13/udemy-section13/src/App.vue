60c89f6372631da43018762cbaf71b3e525a6862
aki158 <hideponpon1931@gmail.com>
2020-12-29 13:58:14 +0900

    187 activeclass属性とexact属性を使ってアクティブなリンクにスタイリングをする

--- section13/udemy-section13/src/App.vue
+++ section13/udemy-section13/src/App.vue
@@ -6,10 +6,14 @@
       <a href="/users">Users</a> -->
       <router-link
         to="/" 
+        active-class="link--active"
+        exact
         class="link"
       >Home</router-link>
       <router-link
         to="users" 
+        active-class="link--active"
+        exact
         class="link"
       >users</router-link>
     </nav>
@@ -20,5 +24,8 @@
 <style scoped>
 .link {
   margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
 }
 </style>
