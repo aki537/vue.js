602051cf15718efbcf7685ee75c66ef0094ee969
aki158 <hideponpon1931@gmail.com>
2020-12-30 17:13:33 +0900

    197 名前付きビューを使って固定されたレイアウトを作成

--- section13/udemy-section13/src/App.vue
+++ section13/udemy-section13/src/App.vue
@@ -1,41 +1,11 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
-    <nav>
-      <!-- aタグを使うと毎回アクセスされるからよくない -->
-      <!-- <a href="/">Home</a>
-      <a href="/users">Users</a> -->
-      <router-link
-        to="/" 
-        active-class="link--active"
-        exact
-        class="link"
-      >Home</router-link>
-      <router-link
-        to="users" 
-        active-class="link--active"
-        exact
-        class="link"
-      >users</router-link>
-    </nav>
-    <!-- <Home></Home> -->
+
+    <router-view name="header"></router-view>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import Home from "./views/Home.vue";
-// export default {
-//   components: {
-//     Home
-//   }
-// }
 </script>
 
-<style scoped>
-.link {
-  margin-right: 10px;
-}
-.link--active {
-  font-size: 20px;
-}
-</style>
