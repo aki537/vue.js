93db7986b8a2302e839aad099c0512f2b65da16c
aki158 <hideponpon1931@gmail.com>
2020-12-30 15:18:55 +0900

    191 ルートコンポーネントの再利用性を損なわないためにpropsをtrueにする

--- section13/udemy-section13/src/views/Users.vue
+++ section13/udemy-section13/src/views/Users.vue
@@ -5,19 +5,20 @@
     <router-link to="/users/1">ユーザー1</router-link>
     <router-link to="/users/2">ユーザー2</router-link>
     <hr>
-    <h1>User No. {{ $route.params.id }}</h1>
+    <h1>User No. {{ id }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  watch: {
-    $route(to, from) {
-      console.log(to);
-      console.log(from);
-      console.log('$routerが変わった');
-    }
-  },
+  props: ["id"],
+  // watch: {
+  //   $route(to, from) {
+  //     console.log(to);
+  //     console.log(from);
+  //     console.log('$routerが変わった');
+  //   }
+  // },
   // created() {
   //   console.log("created");
   // }
