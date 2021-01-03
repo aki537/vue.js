c3ea55f1427269451b4b4b3a6c0c4d3be13a1909
aki158 <hideponpon1931@gmail.com>
2021-01-03 13:47:46 +0900

    211 Vuexのstateを使って実際にコンポーネントから値を取得する

--- section13/udemy-section13/src/views/Home.vue
+++ section13/udemy-section13/src/views/Home.vue
@@ -3,11 +3,17 @@
     <router-view></router-view>
     <h3>Home</h3>
     <button @click="toUsers">Usersのページに行く</button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
   methods: {
     toUsers() {
       this.$router.push({
