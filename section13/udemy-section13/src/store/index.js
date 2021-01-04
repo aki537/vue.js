e27042ed5b2005575234640e74752d5fa658fca8
aki158 <hideponpon1931@gmail.com>
2021-01-04 20:25:48 +0900

    222 getter mutations actionsをそれぞれ別のファイルに切りだし、コードの肥大化を防ぐ

--- section13/udemy-section13/src/store/index.js
+++ section13/udemy-section13/src/store/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import count from "./modules/count";
+import getters from "./getters"
+import mutations from "./mutations"
+import actions from "./actions"
 
 Vue.use(Vuex);
 
@@ -8,34 +11,9 @@ export default new Vuex.Store({
   state: {
     message: ""
   },
-  getters: {
-
-    message: state => state.message
-  },
-  // stateの値を変えるときはmutationで変える
-  mutations: {
-    
-    updateMessage(state, newMessage) {
-      state.message = newMessage;
-    }
-  },
-  // 非同期処理をするときはactionsを使う
-  // （mutationsで非同期処理はかけない)
-  actions: {
-    // increment(context, number) {
-    //   context.commit('increment', number);
-    // },
-    // 下の書き方でcontextを省略もできる
-    
-    updateMessage({ commit }, newMessage) {
-      commit("updateMessage", newMessage);
-    }
-    // incrementAsync(context) {
-    //   setTimeout(() => {
-
-    //   },1000)
-    // }
-  },
+  getters,
+  mutations,
+  actions,
   modules: {
     count
   }
